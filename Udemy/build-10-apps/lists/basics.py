2e4a2fd16b7fcff65f97c6c802c685b022ccc6d6
Zeljko Stjepanovic <brodmj@yahoo.com>
2022-02-17 21:05:15 +0100

    add new excercise

---
+++ Udemy/build-10-apps/lists/basics.py
@@ -0,0 +1,11 @@
+monday_temperatures = [9.1, 8.8, 7.5]
+print(monday_temperatures)
+
+monday_temperatures.append(8.1)
+print(monday_temperatures)
+
+print(monday_temperatures.index(8.1))
+
+monday_temperatures.clear()
+monday_temperatures.append(101.1)
+print(monday_temperatures)
