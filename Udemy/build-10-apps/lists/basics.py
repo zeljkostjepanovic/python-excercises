0cf2ff0aa4511168a137eac5767d32a56c51743c
Zeljko Stjepanovic <brodmj@yahoo.com>
2022-02-20 12:09:07 +0100

    adding some more list method excercises

--- Udemy/build-10-apps/lists/basics.py
+++ Udemy/build-10-apps/lists/basics.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 monday_temperatures = [9.1, 8.8, 7.5]
 print(monday_temperatures)
 
@@ -6,6 +7,13 @@ print(monday_temperatures)
 
 print(monday_temperatures.index(8.1))
 
-monday_temperatures.clear()
 monday_temperatures.append(101.1)
+monday_temperatures.append(88.3)
 print(monday_temperatures)
+print(monday_temperatures[1:4])
+print(monday_temperatures[-4:-1])
+
+my_hello_string = 'hello'
+print(my_hello_string[1])
+print(my_hello_string[-1])
+print(my_hello_string[:3])
