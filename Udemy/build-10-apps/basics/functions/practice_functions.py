115097cca15b4553b47dd1207c9a700c45c48297
Zeljko Stjepanovic <brodmj@yahoo.com>
2022-03-02 11:02:26 +0100

    rename file for functions practice

---
+++ Udemy/build-10-apps/basics/functions/practice_functions.py
@@ -0,0 +1,20 @@
+def mean(mylist):
+    """Calculate the average"""
+    return sum(mylist) / len(mylist)
+
+
+print(mean([1, 3, 4, 5, 3, 56, 6, 3, 2, 334, 2, 3, 445]))
+
+
+def calculate_length(lst):
+    """Return the lenght of the list."""
+    return len(lst)
+
+print(calculate_length([7,4,3,4,5]))
+
+def squared(x):
+    """Return the number squared, with decimals."""
+    return float(x)**2
+
+print(squared(3.23))
+
