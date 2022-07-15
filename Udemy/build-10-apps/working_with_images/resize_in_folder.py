6b54eee851eb15e9ffc4e1d0c50c99761a294110
Zeljko Stjepanovic <brodmj@yahoo.com>
2022-07-15 21:49:03 +0200

    support square shape

--- Udemy/build-10-apps/working_with_images/resize_in_folder.py
+++ Udemy/build-10-apps/working_with_images/resize_in_folder.py
@@ -15,7 +15,7 @@ def longer(x,y):
     elif x < y:
         return y
     else:
-        None
+        return x
 
 def shorter(x,y):
     if x < y:
@@ -23,21 +23,24 @@ def shorter(x,y):
     elif x > y:
         return y
     else:
-        None
+        return x
 
 def resize_images(path, x, y):
     for img in os.listdir(path):
         if img.endswith(".png") or img.endswith(".jpg"):
             image = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
             if detect_orientation(image) == "horizontal":
-                resized_image = cv2.resize(image,(int(longer(x,y)),int(shorter(x,y))), interpolation=cv2.INTER_CUBIC)
+                resized_image = cv2.resize(image,(int(longer(x,y)),int(shorter(x,y))), 
+                interpolation=cv2.INTER_CUBIC)
             elif detect_orientation(image) == "vertical":
-                resized_image = cv2.resize(image,(int(shorter(x,y)),int(longer(x,y))), interpolation=cv2.INTER_CUBIC)
+                resized_image = cv2.resize(image,(int(shorter(x,y)),int(longer(x,y))), 
+                interpolation=cv2.INTER_CUBIC)
             else:
-                resized_image = cv2.resize(image,(int(longer(x,y)),int(longer(x,y))), interpolation=cv2.INTER_CUBIC)
+                resized_image = cv2.resize(image,(int(longer(x,y)),int(longer(x,y))), 
+                interpolation=cv2.INTER_CUBIC)
             cv2.imwrite(os.path.join(path,"resized_{}".format(img)), resized_image)
         else:
             continue
 
 
-resize_images("sample", 1024, 768)
+resize_images("sample", 1440, 1024)
