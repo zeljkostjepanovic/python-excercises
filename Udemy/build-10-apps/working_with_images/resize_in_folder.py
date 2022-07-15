de06944deb82fbe21dd05058815548d7cb29acb6
Zeljko Stjepanovic <brodmj@yahoo.com>
2022-07-15 21:29:29 +0200

    opencv resize all images in folder, keep aspect

---
+++ Udemy/build-10-apps/working_with_images/resize_in_folder.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+
+def detect_orientation(img):
+    if img.shape[1] > img.shape[0]:
+        return "horizontal"
+    elif img.shape[1] == img.shape[0]:
+        return "square"
+    else:
+        return "vertical"
+
+def longer(x,y):
+    if x > y:
+        return x
+    elif x < y:
+        return y
+    else:
+        None
+
+def shorter(x,y):
+    if x < y:
+        return x
+    elif x > y:
+        return y
+    else:
+        None
+
+def resize_images(path, x, y):
+    for img in os.listdir(path):
+        if img.endswith(".png") or img.endswith(".jpg"):
+            image = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
+            if detect_orientation(image) == "horizontal":
+                resized_image = cv2.resize(image,(int(longer(x,y)),int(shorter(x,y))), interpolation=cv2.INTER_CUBIC)
+            elif detect_orientation(image) == "vertical":
+                resized_image = cv2.resize(image,(int(shorter(x,y)),int(longer(x,y))), interpolation=cv2.INTER_CUBIC)
+            else:
+                resized_image = cv2.resize(image,(int(longer(x,y)),int(longer(x,y))), interpolation=cv2.INTER_CUBIC)
+            cv2.imwrite(os.path.join(path,"resized_{}".format(img)), resized_image)
+        else:
+            continue
+
+
+resize_images("/home/zb/Documents/Tutorials/Python/Udemy/build-10-apps/working_with_images/sample", 1024, 768)
