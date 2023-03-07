cab7ccbd32c1c00bb45e900e2bed4949a83d98e8
Zeljko Stjepanovic <brodmj@yahoo.com>
2023-03-07 10:36:21 +0100

    Add message for unknown command and list of tasks

--- Udemy/build-10-apps/todo-list/main.py
+++ Udemy/build-10-apps/todo-list/main.py
@@ -10,15 +10,18 @@ todos = []
 #         tasks.append(task)
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show or exit: ").lower().strip()
     
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for i in enumerate(todos, start=1):
+                print(i[0],i[1])
         case 'exit':
             exit()
+        case other:
+            print("I didnt' understand that.")
     
 print(todos)
