4ea4aad40cbdb1faa2481d198ba1e5709f32f705
Zeljko Stjepanovic <brodmj@yahoo.com>
2023-07-14 13:23:12 +0200

    implement add todo button

--- Udemy/build-10-apps/todo-list/gui.py
+++ Udemy/build-10-apps/todo-list/gui.py
@@ -1,10 +1,32 @@
 from modules import functions
 import PySimpleGUI as sg
+import os
+
+### Make sure to run the program in the same working directory where the script is located
+current_dir = os.path.dirname(__file__)
+os.chdir(current_dir)
 
 label = sg.Text('Type in a task')
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button('Add')
-window = sg.Window('My TODOS', layout=[[label],
-                                       [input_box,add_button]])
-window.read()
+
+
+window = sg.Window('My TODOS', 
+                   layout=[[label], [input_box, add_button]], 
+                   font=("Sans", 10))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
