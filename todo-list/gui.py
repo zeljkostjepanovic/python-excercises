e95d2008de4e6b0ac223eda7b467f8346649524d
Zeljko Stjepanovic <brodmj@yahoo.com>
2023-07-19 15:30:13 +0200

    implement complete and exit buttons

--- todo-list/gui.py
+++ todo-list/gui.py
@@ -13,10 +13,13 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 
 edit_button = sg.Button('Edit')
+complete_button = sg.Button('Complete')
+exit_button = sg.Button('Exit')
 
 layout = [[label],
           [input_box, add_button],
-          [list_box,edit_button],]
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 
 window = sg.Window('My TODOS', 
                    layout=layout, 
@@ -47,6 +50,17 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            index = todos.index(todo_to_complete)
+            todos.pop(index)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            
+        case 'Exit':
+            break
+        
         case 'todos':
             window['todo'].update(value=values['todos'][0])
              
